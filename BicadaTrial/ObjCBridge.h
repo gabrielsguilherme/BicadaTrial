9c91c0017819acf3b57022f7e6192ea88fd6e652
Gabriel Guilherme <gabriel@cppcoder.com>
2016-11-02 23:00:03 -0200

    first version
    building successful

---
+++ BicadaTrial/ObjCBridge.h
@@ -0,0 +1,16 @@
+//
+//  ObjCBridge.h
+//  BicadaTrial
+//
+//  Created by Gabriel Guilherme on 11/2/16.
+//  Copyright © 2016 Gabriel Guilherme. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+
+#endif /* ObjCBridge_h */
+
+
+#import <EstimoteSDK/EstimoteSDK.h>
